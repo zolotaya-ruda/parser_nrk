d8d110c1ba744a6f2645b5021d682eb14424d2be
zolotaya-ruda <75062897+zolotaya-ruda@users.noreply.github.com>
2023-06-27 12:27:57 +0300

    Update connector.py

--- sql_module/connector.py
+++ sql_module/connector.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import Session
 
-engine = create_engine('mysql+pymysql://root:83Linedip@localhost/parser_nkr')
+engine = create_engine('mysql+pymysql://root:@localhost/parser_nkr')
 engine.connect()
 
 base = declarative_base()
